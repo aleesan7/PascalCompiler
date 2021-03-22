3137e1db115f934a2c42e85968625d90e8cb7c47
aleesan7 <ka_sanchezsamayoa@outlook.com>
2021-03-22 12:38:52 -0600

    Version inicial de aplicacion del compilador

---
+++ PascalCompiler/PascalCompiler/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace PascalCompiler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtInputEditor = new System.Windows.Forms.TextBox();
+            this.txtOutputEditor = new System.Windows.Forms.TextBox();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.executeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.translateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewASTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.stStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.syntaxStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.semanticStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 35);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.txtInputEditor);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtOutputEditor);
+            this.splitContainer1.Size = new System.Drawing.Size(747, 803);
+            this.splitContainer1.SplitterDistance = 425;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // txtInputEditor
+            // 
+            this.txtInputEditor.Location = new System.Drawing.Point(12, 12);
+            this.txtInputEditor.Multiline = true;
+            this.txtInputEditor.Name = "txtInputEditor";
+            this.txtInputEditor.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInputEditor.Size = new System.Drawing.Size(723, 358);
+            this.txtInputEditor.TabIndex = 0;
+            // 
+            // txtOutputEditor
+            // 
+            this.txtOutputEditor.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtOutputEditor.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtOutputEditor.Location = new System.Drawing.Point(12, 12);
+            this.txtOutputEditor.Multiline = true;
+            this.txtOutputEditor.Name = "txtOutputEditor";
+            this.txtOutputEditor.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutputEditor.Size = new System.Drawing.Size(723, 322);
+            this.txtOutputEditor.TabIndex = 0;
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            // 
+            // executeToolStripMenuItem
+            // 
+            this.executeToolStripMenuItem.Name = "executeToolStripMenuItem";
+            this.executeToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.executeToolStripMenuItem.Text = "&Execute";
+            // 
+            // translateToolStripMenuItem
+            // 
+            this.translateToolStripMenuItem.Name = "translateToolStripMenuItem";
+            this.translateToolStripMenuItem.Size = new System.Drawing.Size(82, 24);
+            this.translateToolStripMenuItem.Text = "&Translate";
+            // 
+            // viewASTToolStripMenuItem
+            // 
+            this.viewASTToolStripMenuItem.Name = "viewASTToolStripMenuItem";
+            this.viewASTToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.viewASTToolStripMenuItem.Text = "&ViewAST";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.executeToolStripMenuItem,
+            this.translateToolStripMenuItem,
+            this.stStripMenuItem,
+            this.syntaxStripMenuItem,
+            this.semanticStripMenuItem2,
+            this.viewASTToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(747, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // stStripMenuItem
+            // 
+            this.stStripMenuItem.Name = "stStripMenuItem";
+            this.stStripMenuItem.Size = new System.Drawing.Size(146, 24);
+            this.stStripMenuItem.Text = "&ViewSymbolsTable";
+            // 
+            // syntaxStripMenuItem
+            // 
+            this.syntaxStripMenuItem.Name = "syntaxStripMenuItem";
+            this.syntaxStripMenuItem.Size = new System.Drawing.Size(115, 24);
+            this.syntaxStripMenuItem.Text = "&Syntax Report";
+            // 
+            // semanticStripMenuItem2
+            // 
+            this.semanticStripMenuItem2.Name = "semanticStripMenuItem2";
+            this.semanticStripMenuItem2.Size = new System.Drawing.Size(133, 24);
+            this.semanticStripMenuItem2.Text = "&Semantic Report";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 838);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.splitContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtInputEditor;
+        private System.Windows.Forms.TextBox txtOutputEditor;
+        private System.Windows.Forms.ToolStripMenuItem redoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem executeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem translateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewASTToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem stStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem syntaxStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem semanticStripMenuItem2;
+    }
+}
+
---
+++ PascalCompiler/PascalCompiler/Form1.cs
@@ -0,0 +1,364 @@
+﻿//using compiler_app;
+//using CompiPascal.Analysers;
+//using CompiPascal.Utils;
+//using graphviz_cSharp.external;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PascalCompiler
+{
+    public partial class Form1 : Form
+    {
+        //private CompiPascal.Interpreter.Environment globalEnv;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //string inputString = txtInputEditor.Text.ToString();
+
+            //Syntax syntax = new Syntax();
+            //syntax.Analyze(inputString);
+
+            //txtOutputEditor.Text = "";
+
+            //if (!syntax.syntacticErrorsFound) 
+            //{ 
+
+            //    string text = File.ReadAllText("results.txt");
+            //    txtOutputEditor.Text = text;
+
+            //    File.Create("results.txt").Close();
+
+            //    if (syntax.errorsList.Count > 0) 
+            //    {
+            //        foreach(PascalError error in syntax.errorsList) 
+            //        {
+            //            txtOutputEditor.Text += error.GetMesage();
+            //            txtOutputEditor.Text += Environment.NewLine;
+            //        }
+
+            //        GenerateSemanticErrors(syntax.errorsList);
+            //    }
+            //}
+            //else 
+            //{
+            //    MessageBox.Show("Syntax errors found, please verify the syntax errors report.");
+            //}
+        } //old execute button
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string inputString = txtInputEditor.Text.ToString();
+
+            //Syntax syntax = new Syntax();
+            //syntax.AnalyzeTranslator(inputString);
+
+            //txtOutputEditor.Text = "";
+
+            //if (!syntax.strResult.Equals("")) 
+            //{
+            //    txtOutputEditor.Text += syntax.strResult;
+            //    txtOutputEditor.Text += Environment.NewLine;
+            //}
+
+            //if (syntax.errorsList.Count > 0)
+            //{
+            //    foreach (CompiPascal.Utils.PascalError error in syntax.errorsList)
+            //    {
+            //        txtOutputEditor.Text += error.GetMesage();
+            //        txtOutputEditor.Text += Environment.NewLine;
+            //    }
+            //}
+        } //old translate button
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+                //try
+                //{
+                //    //String fileInputPath = "ast.txt";
+                //    //String filename = Path.GetFileName(fileInputPath);
+                //    //MessageBox.Show(filename);
+
+                //    String graphVizString = @"" + File.ReadAllText("ast.txt");
+                //    Bitmap bm = FileDotEngine.Run(graphVizString);
+
+                //    for (int x = 0; x < bm.Width; x++)
+                //    {
+                //        for (int y = 0; y < bm.Height; y++)
+                //        {
+                //            Color clr = bm.GetPixel(x, y);
+                //            Color newClr = Color.FromArgb(clr.R, 0, 0);
+                //        }
+                //    }
+                //    bm.Save(@"ast.jpg");
+                //    MessageBox.Show("AST Image generation completed!");
+                //    //ProcessStartInfo startInfo = new ProcessStartInfo("dot.exe");
+                //    //String action  = "-Tpng ast.txt -o graph.png";
+                //    //startInfo.Arguments = action;
+                //    //Process.Start(startInfo);
+
+                //}
+                //catch (Exception exception)
+                //{
+                //    MessageBox.Show("Error al generar archivos\n" + exception.Message);
+                //}
+            
+        }
+
+        //private void GenerateTablaSimbolosGeneral(CompiPascal.Interpreter.Environment env) 
+        //{
+            //CompiPascal.Interpreter.Environment actual = env;
+            //string content = string.Empty;
+
+            ////while (actual != null)
+            ////{
+            // content += "<html>\n <body> <h2>Compi Pascal Symbols Table</h2> <table style=\"width:100%\" border=\"1\"> <tr><th>Name</th><th>Object Type</th><th>Type</th><th>Environment</th><th>Line</th><th>Column</th></tr> \n";
+
+            //Dictionary<string, CompiPascal.Interpreter.Symbol> variables = actual.GetVariables();
+            //Dictionary<string, CompiPascal.Interpreter.Function> functions = actual.GetFunctions();
+            //Dictionary<string, CompiPascal.Interpreter.Procedure> procedures = actual.GetProcedures();
+
+            //foreach (KeyValuePair<string, CompiPascal.Interpreter.Symbol> variable in variables)
+            //{
+            //    content += "<tr>" +
+            //        "<td>" + variable.Key +
+            //        "</td>" +
+            //        "<td>" + "VARIABLE" +
+            //        "</td>" +
+            //        "<td>" + variable.Value.type.type.ToString() +
+            //        "</td>" +
+            //        "<td>" + actual.GetEnvName() +
+            //        "</td>" +
+            //        "<td>" + variable.Value.line.ToString() +
+            //        "</td>" +
+            //        "<td>" + variable.Value.column.ToString() +
+            //        "</td>" +
+            //        "</tr>";
+            //}
+
+            //foreach (KeyValuePair<string, CompiPascal.Interpreter.Function> function in functions)
+            //{
+            //    content += "<tr>" +
+            //        "<td>" + function.Key +
+            //        "</td>" +
+            //        "<td>" + "FUNCTION" +
+            //        "</td>" +
+            //        "<td>" + function.Value.GetFunctionType().ToString() +
+            //        "</td>" +
+            //        "<td>" + actual.GetEnvName() +
+            //        "</td>" +
+            //        "<td>" + function.Value.line.ToString() +
+            //        "</td>" +
+            //        "<td>" + function.Value.column.ToString() +
+            //        "</td>" +
+            //        "</tr>";
+            //}
+
+            //foreach (KeyValuePair<string, CompiPascal.Interpreter.Procedure> procedure in procedures)
+            //{
+            //    content += "<tr>" +
+            //        "<td>" + procedure.Key +
+            //        "</td>" +
+            //        "<td>" + "PROCEDURE" +
+            //        "</td>" +
+            //        "<td>" + "VOID" +
+            //        "</td>" +
+            //        "<td>" + actual.GetEnvName() +
+            //        "</td>" +
+            //        "<td>" + procedure.Value.line.ToString() +
+            //        "</td>" +
+            //        "<td>" + procedure.Value.column.ToString() +
+            //        "</td>" +
+            //        "</tr>";
+            //}
+
+            //content += "</table> </body> </html>";
+
+            ////actual = actual.GetParent();
+            ////}
+
+            //using (StreamWriter outputFile = new StreamWriter("GlobalSymbolsTable" + "_" + actual.GetEnvName() + "_" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".html"))
+            //{
+            //    outputFile.WriteLine(content);
+            //}
+
+        //}
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            //switch (e.ClickedItem.Text) 
+            //{
+            //    case "&Execute":
+            //        if (!txtInputEditor.Text.Equals("")) 
+            //        {
+            //            Execute();
+            //        }
+            //        else 
+            //        {
+            //            MessageBox.Show("Please enter the code you want to execute.");
+            //        }
+                    
+            //        break;
+            //    case "&Translate":
+            //        if (!txtInputEditor.Text.Equals(""))
+            //        {
+            //            Translate();
+            //        }
+            //        else 
+            //        {
+            //            MessageBox.Show("Please enter the code you want to translate.");
+            //        }
+            //        break;
+            //    case "&Semantic Report":
+            //        //System.Diagnostics.Process.Start("SemanticErrorsReport.html");
+            //        break;
+            //    case "&ViewSymbolsTable":
+            //        if (this.globalEnv == null) 
+            //        {
+            //            MessageBox.Show("You must first execute some code in order to generate the symbols table.");
+            //        }
+            //        else 
+            //        {
+            //            GenerateTablaSimbolosGeneral(this.globalEnv);
+            //        }
+            //        break;
+            //    case "&ViewAST":
+            //        if (this.globalEnv == null)
+            //        {
+            //            MessageBox.Show("You must first execute some code in order to generate the AST");
+            //        }
+            //        else 
+            //        {
+            //            GenerateAST();
+            //        }
+            //        break;
+            //    case "&Exit":
+            //        Application.Exit();
+            //        break;
+            //}
+        }
+
+        private void Execute() 
+        {
+            //string inputString = txtInputEditor.Text.ToString();
+
+            //Syntax syntax = new Syntax();
+            //syntax.Analyze(inputString);
+
+            //txtOutputEditor.Text = "";
+
+            //if (!syntax.syntacticErrorsFound)
+            //{
+            //    this.globalEnv = syntax.globalEnv;
+            //    string text = File.ReadAllText("results.txt");
+            //    txtOutputEditor.Text = text;
+
+            //    File.Create("results.txt").Close();
+
+            //    if (syntax.errorsList.Count > 0)
+            //    {
+            //        foreach (PascalError error in syntax.errorsList)
+            //        {
+            //            txtOutputEditor.Text += error.GetMesage();
+            //            txtOutputEditor.Text += Environment.NewLine;
+            //        }
+
+            //        GenerateSemanticErrors(syntax.errorsList);
+            //    }
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Syntax errors found, please verify the syntax errors report.");
+            //}
+        }
+
+        private void Translate() 
+        {
+            //string inputString = txtInputEditor.Text.ToString();
+
+            //Syntax syntax = new Syntax();
+            //syntax.AnalyzeTranslator(inputString);
+
+            //txtOutputEditor.Text = "";
+
+            //if (!syntax.strResult.Equals(""))
+            //{
+            //    txtOutputEditor.Text += syntax.strResult;
+            //    txtOutputEditor.Text += Environment.NewLine;
+            //}
+        }
+
+        //private void GenerateSemanticErrors(List<PascalError> semanticErrors)
+        //{
+            //string errors = "<html>\n <body> <h2>Compi Pascal Errors</h2> <table style=\"width:100%\" border=\"1\"> <tr> <th>Type</th> <th>Error Description</th> <th>row</th> <th>column</th></tr> \n";
+
+            //foreach (CompiPascal.Utils.PascalError error in semanticErrors)
+            //{
+            //    errors += "<tr>" +
+            //            "<td>" + error.GetType() +
+            //            "</td>" +
+            //            "<td>" + error.GetMesage() +
+            //            "</td>" +
+            //            "<td>" + error.GetLine() +
+            //            "</td>" +
+            //            "<td>" + error.GetColumn() +
+            //            "</td>" +
+            //            "</tr>";
+            //}
+
+            //errors += "</table> </body> </html>";
+            //using (StreamWriter outputFile = new StreamWriter("SemanticErrorsReport.html"))
+            //{
+            //    outputFile.WriteLine(errors);
+            //}
+        //}
+
+        private void GenerateAST() 
+        {
+            //try
+            //{
+            //    //String fileInputPath = "ast.txt";
+            //    //String filename = Path.GetFileName(fileInputPath);
+            //    //MessageBox.Show(filename);
+
+            //    String graphVizString = @"" + File.ReadAllText("ast.txt");
+            //    Bitmap bm = FileDotEngine.Run(graphVizString);
+
+            //    for (int x = 0; x < bm.Width; x++)
+            //    {
+            //        for (int y = 0; y < bm.Height; y++)
+            //        {
+            //            Color clr = bm.GetPixel(x, y);
+            //            Color newClr = Color.FromArgb(clr.R, 0, 0);
+            //        }
+            //    }
+            //    bm.Save(@"ast.jpg");
+            //    MessageBox.Show("AST Image generation completed!");
+            //    //ProcessStartInfo startInfo = new ProcessStartInfo("dot.exe");
+            //    //String action  = "-Tpng ast.txt -o graph.png";
+            //    //startInfo.Arguments = action;
+            //    //Process.Start(startInfo);
+
+            //}
+            //catch (Exception exception)
+            //{
+            //    MessageBox.Show("Error al generar archivos\n" + exception.Message);
+            //}
+        }
+    }
+}
